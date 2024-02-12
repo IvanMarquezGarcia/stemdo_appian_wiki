6c8037d799afb02441345d1271cc3b59a53eee1d
Iván <imarquez@stemdo.io>
2024-02-12 10:22:55 +0100

    chore: add flowbite and edit colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,12 +1,37 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: 'class',
+	content: [
+		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+		'./node_modules/flowbite/**/*.js'
+	],
 	theme: {
 		extend: {
 			colors: {
-				'azul-stemdo': '#4237ff',
+				'stemdo-blue': {
+					light: '#8680ff',
+					original: '#4237FF',
+					dark: '#0d00ff'
+				},
+				'stemdo-purple': {
+					light: '#b699ff',
+					original: '#8859FF',
+					dark: '#5b1aff'
+				},
+				'stemdo-red': {
+					light: '#f35961',
+					original: '#EF2630',
+					dark: '#be0e17'
+				},
+				'stemdo-green': {
+					light: '00ffea',
+					original: '#00C2B1',
+					dark: '#008075'
+				},
 			  },
 		},
 	},
-	plugins: [],
+	plugins: [
+		require('flowbite/plugin')
+	],
 }
